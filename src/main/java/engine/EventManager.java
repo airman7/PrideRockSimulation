cee1973f5b8f01d37bd7c65951a49495a3b7669f
Tanmay Vijayvargyia <12tanmayvijay@gmail.com>
2019-08-31 16:12:24 +0530

    Basic files

---
+++ src/main/java/engine/EventManager.java
@@ -0,0 +1,13 @@
+package engine;
+
+import java.util.Map;
+
+public class EventManager {
+  public void register(String eventName, EventListener listener) {
+
+  }
+
+  public void sendEvent(String eventName, Map<String, Object> context) {
+
+  }
+}
